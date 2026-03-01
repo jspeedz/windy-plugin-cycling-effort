04fd68ba3dbafcd6313582abbfd06b084509f5a7
jspeedz <6095226+jspeedz@users.noreply.github.com>
2026-03-01 10:00:41 +0100

    Add repository link to pluginConfig and update .gitignore to include .idea directory

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -6,6 +6,7 @@ export default {
   description:
     'Evaluates uploaded GPX/KML/GeoJSON routes using route profile and Windy weather data. A "route effort" number will be asigned based on the elevation change and weather conditions. You can use this number to compare weather conditions and decide on when to go out cycling.',
   author: "jspeedz",
+  repository: 'https://github.com/jspeeds/windy-plugin-cycling-effort',
   desktopUI: "embedded",
   mobileUI: "fullscreen",
   routerPath: "/cycling-effort",
