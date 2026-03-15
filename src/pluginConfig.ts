1b29d5389421acc2d412659d1c349e3713dad6d0
jspeedz <6095226+jspeedz@users.noreply.github.com>
2026-03-15 08:02:19 +0100

    Add `private` flag and `homepage` URL to plugin configuration

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -5,8 +5,10 @@ export default {
   icon: "🚴",
   description:
     'Evaluates uploaded GPX/KML/GeoJSON routes using route profile and Windy weather data. A "route effort" number will be asigned based on the elevation change and weather conditions. You can use this number to compare weather conditions and decide on when to go out cycling.',
+  private: true,
   author: "jspeedz",
   repository: 'https://github.com/jspeeds/windy-plugin-cycling-effort',
+  homepage: 'https://github.com/jspeeds/windy-plugin-cycling-effort',
   desktopUI: "embedded",
   mobileUI: "fullscreen",
   routerPath: "/cycling-effort",
